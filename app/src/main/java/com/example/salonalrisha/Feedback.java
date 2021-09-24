e0cafd15a319c252d6c2c9d91e101bd7ace175c2
saduni <saduniudeshika0@gmail.com>
2021-09-24 22:29:53 +0530

    Modify Feedback.java

--- app/src/main/java/com/example/salonalrisha/Feedback.java
+++ app/src/main/java/com/example/salonalrisha/Feedback.java
@@ -4,20 +4,28 @@ public class Feedback {
     private String name;
     private String comment;
     private String email;
+    private String image;
 
 
     public Feedback(){}
-    public Feedback(String name, String comment, String email) {
+    public Feedback(String name, String comment, String email, String image) {
         this.name = name;
         this.comment = comment;
         this.email = email;
+        this.image = image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public void setComment(String comment) {
+
         this.comment = comment;
     }
 
@@ -26,15 +34,22 @@ public class Feedback {
     }
 
     public String getName() {
+
         return name;
     }
 
     public String getComment() {
+
         return comment;
     }
 
     public String getEmail() {
+
         return email;
+    }
+
+    public String getImage() {
+        return image;
     }
 
 }
