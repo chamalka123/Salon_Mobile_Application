3b9c61a260c6fb608966a1423db1eef1973d2d37
saduni <saduniudeshika0@gmail.com>
2021-09-29 12:36:19 +0530

    Create feedback average calculation UI

---
+++ app/src/main/java/com/example/salonalrisha/AverageCalcRating.java
@@ -0,0 +1,14 @@
+package com.example.salonalrisha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AverageCalcRating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_average_calc_rating);
+    }
+}
